11506798d854ff86ffa5eec3baf9e2bb3df0bedc
Mahmoud Ashraf Mahmoud <38630967+uptotec@users.noreply.github.com>
2021-04-20 00:22:13 +0200

    style(web): :art: added svu logo to the login page

--- packages/web/src/modules/login/ui/loginPage/LoginPage.tsx
+++ packages/web/src/modules/login/ui/loginPage/LoginPage.tsx
@@ -5,7 +5,7 @@ import { Redirect } from 'react-router-dom';
 
 import styles from './loginPage.module.css';
 import { LoginForm } from '../loginForm/loginForm';
-// import logo from '../../../../assets/logo-colors.svg';
+import logo from '../../../../assets/svu.svg';
 
 export const LoginPage = (props: LoginControllerData) => {
   if (props.success) {
@@ -16,8 +16,8 @@ export const LoginPage = (props: LoginControllerData) => {
     <Row className={styles.Container}>
       <Col flex={1} className={styles.LogoFormCol}>
         <div className={styles.LogoForm}>
-          {/* <img src={logo} alt="logo" className={styles.Logo} /> */}
-          <h1 style={{ fontSize: 34, margin: '20px auto' }}>I-Learn</h1>
+          <img src={logo} alt="logo" className={styles.Logo} />
+          <h1 style={{ fontSize: 34, margin: '5px auto' }}>I-Learn</h1>
           <LoginForm {...props} />
         </div>
       </Col>
